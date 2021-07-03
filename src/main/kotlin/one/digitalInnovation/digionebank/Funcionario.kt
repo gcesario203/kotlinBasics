42fb9b8700992992d8412a5e4ba22d237c05d93b
Gabriel Cesario <cesario203@outlook.com>
2021-07-03 01:02:46 -0300

    Inicio das aulas ^2

---
+++ src/main/kotlin/one/digitalInnovation/digionebank/Funcionario.kt
@@ -0,0 +1,12 @@
+package one.digitalInnovation.digionebank
+
+import java.math.BigDecimal
+
+abstract class Funcionario(
+    nome : String,
+    cpf: String,
+    val salario : BigDecimal
+    ) : Pessoa(nome, cpf)
+{
+        abstract fun calcularAuxilio()
+}
